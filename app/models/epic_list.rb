1c67a29556c5de5368f4484330e7f00e035ebbbb
Czarek <cezary.karas@elpassion.pl>
2019-04-04 16:01:54 +0200

    Add epics and epic_list to admin

--- app/models/epic_list.rb
+++ app/models/epic_list.rb
@@ -1,3 +1,5 @@
 class EpicList < ApplicationRecord
   validates :name, presence: true
+
+  has_and_belongs_to_many :epics
 end
--- spec/models/epic_spec.rb
+++ spec/models/epic_spec.rb
@@ -1,11 +1,15 @@
 RSpec.describe Epic, type: :model do
-  let(:epic_list) { create(:epic_list) }
+  let(:epic_list) { create_list(:epic_list, 2) }
 
   it 'validates epic list presence' do
-    expect(Epic.new(name: 'some name')).not_to be_valid
+    expect(Epic.new(name: 'some name')).to be_valid
   end
 
   it 'validates name' do
-    expect(Epic.new(name: '', epic_list: epic_list)).not_to be_valid
+    expect(Epic.new(name: '')).not_to be_valid
+  end
+
+  it 'is valid object with epic list' do
+    expect(Epic.new(name: '', epic_lists: epic_list)).not_to be_valid
   end
 end
