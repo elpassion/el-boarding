4f39a4b824d27b110a6d0ecd7f7a67f02d754fd5
Przemek <18726317+ppietrasik@users.noreply.github.com>
2019-04-05 13:07:28 +0200

    change tasks to user tasks

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -1,5 +1,20 @@
 class Types::TaskType < Types::BaseObject
   field :id, Integer, null: false
-  field :name, String, null: false
-  field :description, String, null: false
+  field :name, String, null: true
+  field :description, String, null: true
+  field :done, Boolean, null: true
+
+  def done
+    user_task&.done
+  end
+
+  private
+
+  def user_task
+    @user_task ||= UserTask.find_by(user: user, task: object)
+  end
+
+  def user
+    User.first
+  end
 end
