b9931f18dff1861c7e4e3f38a14f3035933ca83f
Mateusz Zdanowicz <zmateusz@users.noreply.github.com>
2019-08-29 17:03:13 +0200

    Add switch_user gem

--- Gemfile
+++ Gemfile
@@ -61,6 +61,9 @@ group :development do
   gem 'rubocop', require: false
 end
 
+group :staging, :development do
+  gem 'switch_user'
+end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :authenticate_should_be_skipped?
+
+  private
+
+  def authenticate_should_be_skipped?
+    params[:controller] == 'switch_user'
+  end
 end
